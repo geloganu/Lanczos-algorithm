fdc3f41af40b69811f4892643d08584bd5c360d6
geloganu <91586176+geloganu@users.noreply.github.com>
2022-07-13 13:39:18 -0500

    built initial iteration

--- lanczos.py
+++ lanczos.py
@@ -1,17 +1,48 @@
 import numpy as np
 from numpy import linalg
 
-#lanczos iteration
+#normalization
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
 
-def iterate(H, x, n: int):
+
+#lanczos iteration
+def iterate(H, x, m: int):
     """
     input:
-        H (2D array): Hamiltonian
-        x (1D array): initalization vector
-        n (int): size of system
+        H (nXn array): Hamiltonian
+        x (n 1D array): initalization vector
+        m (int): size of system
 
     output:
         T (2D array): tridiagonal matrix
-        K (2D array): Krylov vector
+        K (2D array): Krylov subspace (equal to Q in tutorial)
+
+        where T = K* H K
     """
+    n = len(H)
+
+    #ensure n>=m
+    if m>n:
+        m = n
+
+    T = np.zeros((m,m))
+    V = np.zeros((n,m))
+    x =  normalize(x) #first krylov vector
+    xt = np.transpose(x) #first krylov vector transpose
+    V[:,0] = xt #first krylov subspace basis vector
+
+    """first iteration"""
+    w = np.dot(H, xt) # new candidate vector
+    alpha = np.dot(xt.conj(), w) #a0 for tridiagonal matrix
+    T[0,0] = alpha
+
+    """subsequent iterations"""
+    for j in range(1,m):
+        w = np.dot()
+
+
 
