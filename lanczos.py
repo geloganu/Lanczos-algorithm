97b04cdec6a43757991f266ed74d660f1812aee8
geloganu <91586176+geloganu@users.noreply.github.com>
2022-07-14 11:49:09 -0500

    completed iteration function

--- lanczos.py
+++ lanczos.py
@@ -36,13 +36,30 @@ def iterate(H, x, m: int):
     V[:,0] = xt #first krylov subspace basis vector
 
     """first iteration"""
-    w = np.dot(H, xt) # new candidate vector
-    alpha = np.dot(xt.conj(), w) #a0 for tridiagonal matrix
+    w = np.dot(H, V[:,0]) # new candidate vector
+    alpha = np.dot(V[:,0], w) #a0 for tridiagonal matrix
     T[0,0] = alpha
+    w = w - alpha*V[:,0] #gram-schmidt 
 
     """subsequent iterations"""
-    for j in range(1,m):
-        w = np.dot()
+    for j in range(1,m-1):
+        beta = np.sqrt(np.dot(w,w))
+        V[:,j] = w / beta
+       
+        #gram schmidt
+        for i in range(j-1):
+            V[:,j] = V[:,j] - np.dot(V[:,j].conj(), V[:i])*V[:i]
 
+        #normalize
+        V[:,j] = normalize(V[:,j])
 
+        w = np.dot(H, V[:,j])
+        alpha = np.dot(V[:,0], w)
+        w = w - alpha*V[:,j] - beta*V[:j-1] 
+
+        T[j,j] = alpha
+        T[j-1,j] = beta
+        T[j,j-1] = beta
+        
+    return T, V
 
